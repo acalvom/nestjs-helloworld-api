dbd66b4d11f4662a77eeeffe6b3a706f0e7f464b
acalvom <andreac.3108@gmail.com>
2022-04-08 14:02:11 +0200

    Create character DTO

---
+++ src/character/characterDto.ts
@@ -0,0 +1,8 @@
+export class CharacterDto {
+    public align: string;
+    public alive: boolean;
+    public identity: string;
+    public name: string;
+    public id: number;
+    public sex: string;
+}
